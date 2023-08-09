cf334359ea1c63c128d990844ce0f223a3a3bfb8
Eduardo Aguilar <es123y123@gmail.com>
2023-08-09 16:16:52 -0500

    Galeria funcionando

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import Swiper from "react-native-swiper";
+
+const images = [
+  { uri: "https://i.imgur.com/52DBcCg.jpg" },
+  { uri: "https://i.imgur.com/7QZMRcV.jpg" },
+  { uri: "https://i.imgur.com/8nBNvc1.jpg" },
+  { uri: "https://i.imgur.com/7QZMRcV.jpg" },
+  { uri: "https://i.imgur.com/sXFSen7.jpg" },
+];
 
 export default function App() {
+  const renderImageItem = (image, index) => {
+    return (
+      <View key={index} style={styles.slide}>
+        <Image source={{ uri: image.uri }} style={styles.image} resizeMode="contain" />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Swiper style={styles.wrapper} loop={false} showsPagination={false}>
+        {images.map((image, index) => renderImageItem(image, index))}
+      </Swiper>
     </View>
   );
 }
@@ -13,8 +31,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  wrapper: {},
+  slide: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
